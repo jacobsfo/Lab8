303f0a302fc84dbef5aaa4322cf87b92f3bd9fbb
Jacob Alamparambil <jacobsfo@gmail.com>
2020-05-14 21:46:47 -0700

    vid

--- turnin/jalam004_lab8_part1.c
+++ turnin/jalam004_lab8_part1.c
@@ -1,9 +1,9 @@
 /*	Author: lab
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 8  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	Link:https://www.youtube.com/watch?v=E-ESg44cjs8
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
